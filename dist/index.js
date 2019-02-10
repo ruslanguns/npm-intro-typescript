80a65caf9f61f5efd826e2507cf8e74e7be535f2
Ruslan Gonzalez <ruslanguns@gmail.com>
2019-02-10 20:18:21 +0000

    Proyecto Node con Typescript

---
+++ dist/index.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function hola() {
+    console.log('Hola Mundo');
+}
+exports.hola = hola;
+function holaPersonalizado(nombre) {
+    console.log(`Hola ${nombre}`);
+}
+exports.holaPersonalizado = holaPersonalizado;
+function holaAlCursoNPM() {
+    console.log('Hola al curso de NPM!');
+}
+exports.holaAlCursoNPM = holaAlCursoNPM;
---
+++ lib/index.ts
@@ -0,0 +1,14 @@
+export function hola() {
+    console.log('Hola Mundo');
+}
+
+export function holaPersonalizado( nombre: string ) {
+    console.log(`Hola ${ nombre }`);
+    
+}
+
+export function holaAlCursoNPM() {
+    console.log('Hola al curso de NPM!');
+    
+}
+
